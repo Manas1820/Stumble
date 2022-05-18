cb77c5b0872512a4718c3646466e2c31dd637f31
manas1820 <manasgupta1820@gmail.com>
2022-05-19 00:08:47 +0530

    feat : Basic Setup

---
+++ database/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"log"
+	"os"
+	"stumble/models"
+
+	"github.com/joho/godotenv" // package used to read the .env file
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Init() *gorm.DB {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	dbURL := os.Getenv("POSTGRES_URL")
+
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.User{})
+
+	return db
+}
